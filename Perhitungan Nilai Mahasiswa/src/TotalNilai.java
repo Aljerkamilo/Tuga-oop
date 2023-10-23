dd9a1e1a519a23cbb4ab1266e6ac0e14517c0c69
Aljerkamilo <148038322+Aljerkamilo@users.noreply.github.com>
2023-10-23 16:34:31 +0700

    Add files via upload

---
+++ Perhitungan Nilai Mahasiswa/src/TotalNilai.java
@@ -0,0 +1,14 @@
+public class TotalNilai {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama: Aljer Kamilo");
+        System.out.println("NIM: 1202220370");
+        float NilaiTubes= ((30 * 80)/100);
+        float NilaiQuiz= ((10 * 95)/100);
+        float NilaiTugas= ((10 * 85)/100 );
+        float NilaiUTS= ((25 * 80)/100);
+        float NilaiUAS= ((25 *75)/100);
+        float NilaiTotal= (NilaiTubes + NilaiQuiz + NilaiTugas + NilaiUTS + NilaiUAS); 
+        System.out.println("Nilai Matakuliah Pemograman Berorientasi Objek : "+NilaiTotal );
+    }
+}
